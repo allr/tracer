3a55c2b224d1eb5aac114fa52e843c9502d2ca41
Ingo Korb <ingo.korb@tu-dortmund.de>
2013-10-28 17:19:17 +0100

    Add a few example R programs

---
+++ demos/mandelbrot.R
@@ -0,0 +1,38 @@
+# resolution and iteration count constants
+XRES = 1000
+YRES = 1000
+ITERS = 200
+
+# "quantize" image
+# (boolean decision if the point escaped or not)
+quant <- function(v) {
+  return(Re(v*v) < 16)
+}
+
+# calculate C value for a given pixel (coordinates between 0 and 1)
+calcC <- function(imag, real) {
+  return((-1 + 2 * imag) * 1i + (-2.3 + 3.0*real))
+}
+
+# calculate a single iteration step
+iterStep <- function(img, pxc) {
+  return(img * img + pxc)
+}
+
+calcMandel <- function() {
+  # create empty image and per-pixel constants
+  image  <- matrix(0+0i, ncol=XRES, nrow=YRES)
+  pixelc <- outer((1:YRES)/YRES, (1:XRES)/XRES, calcC)
+
+  # parallel iteration of all pixels (inefficient)
+  for (i in 1:ITERS) {
+    image <- iterStep(image, pixelc)
+  }
+
+  image
+}
+
+# plot the image
+image <- calcMandel()
+quant(image)
+
---
+++ demos/matmult-byelement.R
@@ -0,0 +1,22 @@
+# by-the-book matrix multiplication - very slow, but nice for demonstrations
+
+randommatrix <- function (n,m) {
+  matrix(rnorm(n*m, mean=1, sd=1), n, m)
+}
+
+SIZE <- 100
+
+a <- randommatrix(SIZE, SIZE)
+b <- randommatrix(SIZE, SIZE)
+
+result <- matrix(0, SIZE, SIZE)
+
+for (i in 1:SIZE) {
+  for (j in 1:SIZE) {
+    result[i,j] <- 0
+    
+    for (k in 1:SIZE) {
+      result[i,j] <- result[i,j] + a[i,k] * b[k,j]
+    }
+  }
+}
